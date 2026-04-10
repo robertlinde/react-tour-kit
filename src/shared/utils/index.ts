730a63c8f6ad8d72c6e13cff399e9e07e2b61999
robertlinde <linde.robert@t-online.de>
2026-04-10 09:08:30 +0200

    fix: truncate dots

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,1 +1,3 @@
 export {calculateTooltipPosition} from './calculate-tooltip-position.util';
+export {getTruncatedDots, MAX_VISIBLE_DOTS, DOT_SIZE, DOT_GAP, DOTS_CONTAINER_WIDTH} from './get-truncated-dots.util';
+export type {DotItem} from './get-truncated-dots.util';
